32dc42b07b02fb2daa9c91ba1b0bd1b648d823e5
Eisss20 <pajenan.j@gmail.com>
2024-10-16 23:15:13 +0700

    feat(landing-page): create nav bar and hero section with responsive design

--- Pajenan-Blog/src/App.jsx
+++ Pajenan-Blog/src/App.jsx
@@ -1,16 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import HLogo from "./assets/IconHH.svg";
+import HambergerNav from "./assets/hambergerbar.svg";
+import Personpic from "./assets/thomaspic.jpg";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function NavBar() {
   return (
-    <section>
-    <h1 className="text-red-500 text-3xl font-bold ">My First React App on Vite</h1>
-    </section>
-  )
+    <nav className="flex items-center justify-between py-4 px-10 bg[#DAD6D1] border-b ">
+      <div>
+        <img src={HLogo} alt="Icon H" className="w-10 h-10" />
+      </div>
+      <div>
+        <img src={HambergerNav} alt="hambergerBar" className="w-5 h-5" />
+      </div>
+    </nav>
+  );
 }
 
-export default App
+function HeroSection() {
+  return (
+    <main className=" py-10 xl:pl-10  bg[#F9F8F6] flex flex-col justify-center  mx-auto  ">
+      <div className=" flex flex-col xl:flex-row items-center xl:pl-10">
+        <article>
+          <section className="  m-5  ">
+            <h1 className="  text-black text-5xl text-center  xl:text-right font-semibold ">
+              {" "}
+              Stay <br className="hidden lg:block" /> Informed, <br /> Stay
+              Inspired{" "}
+            </h1>
+          </section>
+          <section className=" bg[#F9F8F6]">
+            <h2 className=" text-center  xl:text-right font-light text-lg ">
+              {" "}
+              Discover a World of Knowledge at Your Fingertips. Your Daily Dose
+              of Inspiration and Information.{" "}
+            </h2>
+          </section>
+        </article>
+        <img
+          src={Personpic}
+          alt="Personalpic"
+          className="h-[500px] w-[350px] xl:w-[400px]  object-cover rounded-lg shadow-lg xl:w-1/3 mx-4 mb-8 mt-5 xl:mb-0"
+        />
+
+        <section className="py-1  m-4 ">
+          <div className=" py-2">
+            <span className="text-xs font-normal"> -Author </span>
+            <h3 className="text-lg font-semibold"> Thompson P. </h3>
+          </div>
+
+          <section className="py-2">
+            <p>
+              {" "}
+              I am a pet enthusiast and freelance writer who specializes in
+              animal behavior and care. With a deep love for cats, I enjoy
+              sharing insights on feline companionship and wellness.
+            </p>
+            <p className="pt-5">
+              {" "}
+              When i’m not writing, I spends time volunteering at my local
+              animal shelter, helping cats find loving homes.{" "}
+            </p>
+          </section>
+        </section>
+      </div>
+    </main>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <HeroSection />
+    </>
+  );
+}
+
+export default App;
