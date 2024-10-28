6462eb2e11a31a55e0e258d2d059a170a70280e7
Eisss20 <pajenan.j@gmail.com>
2024-10-28 20:55:11 +0700

    Feat(BlogCard):implement adjust the blogcard data and set selecting button by render list concerpt

--- Pajenan-Blog/src/components/ArticleSection.jsx
+++ Pajenan-Blog/src/components/ArticleSection.jsx
@@ -43,6 +43,7 @@ export function BlogCard({image,category,title,description,author,date}) {
   }
   
 export function ArticleSection() {
+  const categories = ["Highlight", "Cat", "Inspiration", "General"];
   return (
     <>
     <h1 className="pl-5 font-semibold text-3xl  my-3"> Latest articles </h1>
@@ -61,9 +62,13 @@ export function ArticleSection() {
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
-            <SelectItem value="highlight">Highlight</SelectItem>
-            <SelectItem value="category2">Category 2</SelectItem>
-            <SelectItem value="category3">Category 3</SelectItem>
+          {categories.map((item,index) => {
+                return (
+                  <SelectItem  value={index}>
+                    {item}
+                  </SelectItem>
+                );
+              })}
           </SelectGroup>
         </SelectContent>
       </Select>
@@ -72,54 +77,18 @@ export function ArticleSection() {
     </div>
     {/* เรนเดอร์จาก data */}
     <article className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-0">
-        <BlogCard
-          image={blogPosts[0].image}
-          category={blogPosts[0].category}
-          title={blogPosts[0].title}
-          description={blogPosts[0].description}
-          author={blogPosts[0].author}
-          date={blogPosts[0].date}
-        />
-        <BlogCard
-          image={blogPosts[1].image}
-          category={blogPosts[1].category}
-          title={blogPosts[1].title}
-          description={blogPosts[1].description}
-          author={blogPosts[1].author}
-          date={blogPosts[1].date}
-        />
-        <BlogCard
-          image={blogPosts[2].image}
-          category={blogPosts[2].category}
-          title={blogPosts[2].title}
-          description={blogPosts[2].description}
-          author={blogPosts[2].author}
-          date={blogPosts[2].date}
-        />
-        <BlogCard
-          image={blogPosts[3].image}
-          category={blogPosts[3].category}
-          title={blogPosts[3].title}
-          description={blogPosts[3].description}
-          author={blogPosts[3].author}
-          date={blogPosts[3].date}
-        />
-        <BlogCard
-          image={blogPosts[4].image}
-          category={blogPosts[4].category}
-          title={blogPosts[4].title}
-          description={blogPosts[4].description}
-          author={blogPosts[4].author}
-          date={blogPosts[4].date}
-        />
-        <BlogCard
-          image={blogPosts[5].image}
-          category={blogPosts[5].category}
-          title={blogPosts[5].title}
-          description={blogPosts[5].description}
-          author={blogPosts[5].author}
-          date={blogPosts[5].date}
-        />
+   {blogPosts.map((item,index) => (
+    <BlogCard
+        key={index} 
+        image={item.image}
+        category={item.category}
+        title={item.title}
+        description={item.description}
+        author={item.author}
+        date={item.date}
+    />
+     ))};
+
       </article>
 
 
